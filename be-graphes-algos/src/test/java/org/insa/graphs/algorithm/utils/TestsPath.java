aec9da2f11971a26a1e94c2d7e4cc59bc375b148
Vendeville Leo <lvendevi@insa-20710>
2022-05-18 08:34:57 +0200

    A* : modification des tests et ajout d'un test supplementaire

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/TestsPath.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/TestsPath.java
@@ -12,6 +12,7 @@ import org.insa.graphs.algorithm.ArcInspector;
 import org.insa.graphs.algorithm.ArcInspectorFactory;
 import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.shortestpath.BellmanFordAlgorithm;
+import org.insa.graphs.algorithm.shortestpath.DijkstraAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.AStarAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
 import org.insa.graphs.algorithm.shortestpath.ShortestPathSolution;
@@ -146,6 +147,43 @@ public class TestsPath {
         
         
     }
+    
+    @Test
+    public void testDijsktraAStar() throws IOException {
+        // Visit these directory to see the list of available files on Commetud.
+        final String mapName = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/insa.mapgr";
+        final String correctPathName = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31insa_rangueil_r2.path";
+
+        // Create a graph reader.
+        final GraphReader reader = new BinaryGraphReader(
+                new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
+
+        final Graph graph = reader.read();
+        
+        final PathReader pathReader = new BinaryPathReader(
+                new DataInputStream(new BufferedInputStream(new FileInputStream(correctPathName))));
+
+        final Path correctPath = pathReader.readPath(graph);
+        Node origin = correctPath.getOrigin();
+        Node dest = correctPath.getDestination();
+
+        List<ArcInspector> ai = ArcInspectorFactory.getAllFilters();
+        
+        
+        //    public ShortestPathData(Graph graph, Node origin, Node destination, ArcInspector arcInspector) {
+        ShortestPathData sh = new ShortestPathData(graph, origin, dest, ai.get(0));
+        
+        DijkstraAlgorithm dij = new DijkstraAlgorithm(sh);
+        AStarAlgorithm astar = new AStarAlgorithm(sh);
+        ShortestPathSolution solutionB = dij.run();
+        ShortestPathSolution solutionD = astar.run();
+        
+        
+        assertTrue((solutionB.getPath().compareTo(solutionD.getPath()) == 0));
+        
+        
+    }
+    
     /*
     @Test
     public void testBellmanVelo() throws IOException {
